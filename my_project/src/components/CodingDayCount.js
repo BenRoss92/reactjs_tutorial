75c9c732bdd3eff480af0f846bf9ea166d538608
BenRoss92 <benjaminjackross@gmail.com>
2017-06-20 18:14:58 +0100

    Replace ES6 class components with stateless functional components

--- my_project/src/components/CodingDayCount.js
+++ my_project/src/components/CodingDayCount.js
@@ -1,35 +1,29 @@
-import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-export class CodingDayCount extends Component {
-  percentToDecimal(decimal) {
-    return ((decimal * 100) + '%')
-  }
-  calcGoalProgress(total, goal) {
-    return this.percentToDecimal(total/goal)
-  }
-  render() {
-    return (
-      <div className="coding-day-count">
-        <div className="total-days">
-          <span>{this.props.total}</span>
-          <span>days</span>
-        </div>
-        <div className="coding-days">
-          <span>{this.props.coding}</span>
-          <span>days</span>
-        </div>
-        <div className="non-coding-days">
-          <span>{this.props.nonCoding}</span>
-          <span>non-coding day</span>
-        </div>
-        <div>
-          <span>{this.calcGoalProgress(
-            this.props.total,
-            this.props.goal
-          )}</span>
-        </div>
-      </div>
-    )
-  }
+const percentToDecimal = (decimal) => {
+  return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+  return percentToDecimal(total/goal)
+}
+
+export const CodingDayCount = ({total, coding, nonCoding, goal}) => (
+    <div className="coding-day-count">
+      <div className="total-days">
+        <span>{total}</span>
+        <span>days</span>
+      </div>
+      <div className="coding-days">
+        <span>{coding}</span>
+        <span>days</span>
+      </div>
+      <div className="non-coding-days">
+        <span>{nonCoding}</span>
+        <span>non-coding day</span>
+      </div>
+      <div>
+        <span>{calcGoalProgress(total, goal)}</span>
+      </div>
+    </div>
+)
