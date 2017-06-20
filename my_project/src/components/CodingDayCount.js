80fb6e6e0c06f85a6238a8ccf05de506460f6a4b
BenRoss92 <benjaminjackross@gmail.com>
2017-06-20 18:27:06 +0100

    Add react-icons

--- my_project/src/components/CodingDayCount.js
+++ my_project/src/components/CodingDayCount.js
@@ -1,4 +1,7 @@
 import '../stylesheets/ui.scss'
+import TiCodeOutline from 'react-icons/lib/ti/code-outline'
+import TiMediaPauseOutline from 'react-icons/lib/ti/media-pause-outline'
+import TiCalendarOutline from 'react-icons/lib/ti/calendar-outline'
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -12,14 +15,17 @@ export const CodingDayCount = ({total, coding, nonCoding, goal}) => (
     <div className="coding-day-count">
       <div className="total-days">
         <span>{total}</span>
+          <TiCalendarOutline />
         <span>days</span>
       </div>
       <div className="coding-days">
         <span>{coding}</span>
+          <TiCodeOutline />
         <span>days</span>
       </div>
       <div className="non-coding-days">
         <span>{nonCoding}</span>
+          <TiMediaPauseOutline />
         <span>non-coding day</span>
       </div>
       <div>
