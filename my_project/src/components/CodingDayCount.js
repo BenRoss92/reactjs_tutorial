3673196d1c2dae6a8fc799b6f687f62ce36ceae6
BenRoss92 <benjaminjackross@gmail.com>
2017-06-22 13:01:07 +0100

    Add prop Typechecking validation using PropTypes package

--- my_project/src/components/CodingDayCount.js
+++ my_project/src/components/CodingDayCount.js
@@ -2,6 +2,7 @@ import '../stylesheets/ui.scss'
 import TiCodeOutline from 'react-icons/lib/ti/code-outline'
 import TiMediaPauseOutline from 'react-icons/lib/ti/media-pause-outline'
 import TiCalendarOutline from 'react-icons/lib/ti/calendar-outline'
+import PropTypes from 'prop-types'
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -33,6 +34,15 @@ export const CodingDayCount = ({total=15, coding=10, nonCoding=5, goal=50}) => (
       </div>
     </div>
 )
+
+CodingDayCount.propTypes = {
+  // E.g. 'total' prop value must be a number the value is required (if not given
+  //, will print warning to console):
+  total: PropTypes.number.isRequired,
+  coding: PropTypes.number,
+  nonCoding: PropTypes.number,
+  goal: PropTypes.number
+}
 
 // Can also add default prop values in same way as using ES6 classes
 // - i.e. if 'CodingDayCount' was a class, would add them to the instance
